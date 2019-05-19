c9452f2a48a0c2709ab44757adbfc3c9df8a1de0
Camila <salescamila1@gmail.com>
2019-05-19 15:05:32 -0400

    chore: Warnings resolved

--- readable/src/actions/shared.js
+++ readable/src/actions/shared.js
@@ -1,9 +1,9 @@
 //import { getInitialData } from '../utils/api'
-import { receiveUsers } from '../actions/users'
-import { setAuthedUser } from '../actions/authedUser'
-import { showLoading, hideLoading } from 'react-redux-loading'
+//import { receiveUsers } from '../actions/users'
+//import { setAuthedUser } from '../actions/authedUser'
+import { showLoading/*, hideLoading*/ } from 'react-redux-loading'
 
-const AUTHED_ID = 'camilasales'
+//const AUTHED_ID = 'camilasales'
 
 export function handleInitialData () {
   return (dispatch) => {
