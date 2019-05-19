556ecd70fd0f18101e6acff926ef3b1c5597eb8d
Camila <salescamila1@gmail.com>
2019-05-19 11:31:42 -0400

    Chore: dependencies, middleware, actions, reducers

---
+++ readable/src/actions/shared.js
@@ -0,0 +1,20 @@
+import { getInitialData } from '../utils/api'
+import { receiveUsers } from '../actions/users'
+import { setAuthedUser } from '../actions/authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
+const AUTHED_ID = 'camilasales'
+
+export function handleInitialData () {
+  return (dispatch) => {
+    dispatch(showLoading())
+    return getInitialData()
+      .then(({ users, tweets }) => {
+        dispatch(receiveUsers(users))
+        //dispatch(receiveTweets(posts))
+        //dispatch(receiveTweets(comments))
+        dispatch(setAuthedUser(AUTHED_ID))
+        dispatch(hideLoading())
+      })
+  }
+}
