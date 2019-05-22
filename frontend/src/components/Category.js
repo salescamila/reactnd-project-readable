8364ce6058bd300d4a90dddd226c3dd18649090c
Camila Sales <salescamila1@gmail.com>
2019-05-21 23:02:55 -0400

    feat: Listing all the categories

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,37 +1,34 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Link, withRouter } from 'react-router-dom'
-//import Post from './Post'
-//import NewPost from './NewPost'
+import { Link } from 'react-router-dom'
 
 class Category extends Component {
   render() {
-    const { categories } = this.props.categories
+    const { categories } = this.props.cats
     if (categories === null) {
       return <p>Não existem categorias cadastradas.</p>
     }
 
-    console.log('Categorias: ', categories)
     return (
       <div>
         <h3><u>Lista de Categorias das Postagens.</u></h3>
         <ul>
-            <li key={0}>
-            {/*<Link to={`/category/${cats.path}`}>{cats.name}</Link>*/}
-            </li>
+            {categories
+              ? categories.map((c, i) => { 
+                return <li key={i}><Link to={`/category/${c.path}`}>{c.name}</Link></li> 
+                })
+              : null
+            } 
         </ul>
-
-        {console.log(this.props)}
       </div>
     )
   }
 }
 
-function mapStateToProps ({categories}) {
-  return{
-    categories
+function mapStateToProps ({ categories }) {
+  return {
+    cats: categories
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Category))
-
+export default connect(mapStateToProps)(Category)
